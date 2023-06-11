dc35e1da2fe5c4b831e7adb48f5b3df76e8d5697
mateogall0 <mateogesede@gmail.com>
2023-06-11 18:24:10 -0300

    random_normal function added

--- one_neuron/matrix_operations.c
+++ one_neuron/matrix_operations.c
@@ -4,7 +4,7 @@
 float random_normal()
 {
 	static int initialized = 0;
-    float u1, u2, z;
+    float u1, u2;
 
     if (!initialized) {
         srand(time(NULL));  // Initialize the random number generator
@@ -14,7 +14,5 @@ float random_normal()
     u1 = (float)rand() / RAND_MAX;  // Generate a random number between 0 and 1
     u2 = (float)rand() / RAND_MAX;  // Generate a second random number between 0 and 1
 
-    z = sqrt(-2.0 * log(u1)) * cos(2.0 * M_PI * u2);  // Box-Muller transform
-
-    return z;
+    return sqrt(-2.0 * log(u1)) * cos(2.0 * M_PI * u2);  // Box-Muller transform
 }

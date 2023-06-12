c06195f2c29060c36f22164a62d48f2cd6aa7f35
mateogall0 <mateogesede@gmail.com>
2023-06-12 15:03:11 -0300

    main.h

--- one_neuron/main.h
+++ one_neuron/main.h
@@ -2,7 +2,13 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <math.h>
 
+#define EULER 2.71828182845904523536
+
+float random_normal();
 
 typedef struct matrix
 {
